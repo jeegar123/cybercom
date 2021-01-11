ea14dd3ac515af2a1fcb5777f0e8ac8b7f95da0c
Jeegar Vinodkumar <jeegarvinodkumar123@gmail.com>
2021-01-11 13:38:25 +0530

    Day 2 Of Javascript challenge

---
+++ 11-01-2021/js-coding-challenge-1.js
@@ -0,0 +1,43 @@
+// this challenge is same as 08-01-2021
+
+// score of all teams
+/**
+ *  Storing score in array beczuse multiple values of same team
+ */
+let johnTeamScore = [89, 120, 20];
+let markTeamScore = [145, 94, 12];
+let maryTeamScore = [0, 134, 10];
+
+// average of all teams
+/**
+ *  use reduce method of array ,to take sum of all values of array
+ *
+ */
+let avgOfJohnTeam =
+  johnTeamScore.reduce((a, b) => {
+    return a + b;
+  }, 0) / johnTeamScore.length;
+let avgOfMarkTeam =
+  markTeamScore.reduce((a, b) => {
+    return a + b;
+  }, 0) / markTeamScore.length;
+
+let avgOfMaryTeam =
+  maryTeamScore.reduce((a, b) => {
+    return a + b;
+  }, 0) / maryTeamScore.length;
+
+// comparing all teams avg
+if (
+  avgOfMarkTeam == avgOfJohnTeam &&
+  avgOfMarkTeam == avgOfMaryTeam &&
+  avgOfJohnTeam == avgOfMaryTeam
+) {
+  console.log("match Draw");
+} else if (avgOfJohnTeam > avgOfMarkTeam && avgOfJohnTeam > avgOfMaryTeam) {
+  console.log("Winner is John Team with average " + avgOfJohnTeam);
+} else if (avgOfMarkTeam > avgOfJohnTeam && avgOfMarkTeam > avgOfMaryTeam) {
+  console.log("Winner is Mark Team with average " + avgOfMarkTeam);
+} else {
+  console.log("Winner is Mary Team with average " + avgOfMaryTeam);
+}
