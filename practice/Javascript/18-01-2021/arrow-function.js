e11b107a491bb7e7c53808fba662fae57a284dbe
Jeegar Vinodkumar <jeegarvinodkumar123@gmail.com>
2021-01-18 18:33:02 +0530

    updated

--- practice/Javascript/18-01-2021/arrow-function.js
+++ practice/Javascript/18-01-2021/arrow-function.js
@@ -1,29 +1,47 @@
 let mybtn = {
-    elementName: "My Button",
-    value: 20,
-    clickMe: function () {
-      document.querySelector("button").addEventListener("click", function () {
-          // ES5
+  elementName: "My Button",
+  value: 20,
+  clickMe: function () {
+    document.querySelector("button").addEventListener(
+      "click",
+      function () {
+        // ES5
         // now this function will point to global ,beczuse now sharing in lexical
         let myString = `Element Name ${this.elementName} and value is ${this.value}`;
         alert(myString);
-      }.bind(this));
-    },
-  };
-  
-  // mybtn.clickMe();
-  
-  let mybtn2 = {
-      elementName: "My Button",
-      value: 20,
-      clickMe: function () {
-        document.querySelector("button").addEventListener("click", () => {
-            // ES6
-          // arrow function will share lexical scope for this
-          let myString = `Element Name ${this.elementName} and value is ${this.value}`;
-          alert(myString);
-        });
-      },
-    };
-    
-    mybtn2.clickMe();
+      }.bind(this)
+    );
+  },
+};
+
+// mybtn.clickMe();
+
+let mybtn2 = {
+  elementName: "My Button",
+  value: 20,
+  clickMe: function () {
+    document.querySelector("button").addEventListener("click", () => {
+      // ES6
+      // arrow function will share lexical scope for this
+      let myString = `Element Name ${this.elementName} and value is ${this.value}`;
+      alert(myString);
+    });
+  },
+};
+
+mybtn2.clickMe();
+
+let num = 10;
+
+
+let testObject = {
+    num : 12,
+    testP : () =>{
+      return this.num;
+    }
+}
+
+console.log(testObject.testP());
+
+
+
