18480cd4a36e05f31f5917b3921ac2987318a590
Jeegar Vinodkumar <jeegarvinodkumar123@gmail.com>
2021-02-05 22:32:18 +0530

    ajax with php

---
+++ coding/05-02-2021/registration/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+    <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <form action="" method="post" novalidate>
+
+        <div class="row">
+
+         <div class="form-group">
+            <div class="input-input-group-prepend">
+                
+            </div>
+         </div>
+
+        </div>
+           
+        </form>
+
+    </div>
+</body>
+
+</html>
